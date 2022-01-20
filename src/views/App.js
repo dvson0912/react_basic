ff48f8667873e94c3352609c7a9a52ab564ef2a5
Son Dao <dvson0912@gmail.com>
2022-01-21 00:07:16 +0700

    thay doi

--- src/views/App.js
+++ src/views/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.scss";
+import MyComponent from "./myComponents";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <MyComponent />
         <img src={logo} className="App-logo" alt="logo" />
         <p>sơn Đào</p>
         <a
---
+++ src/views/myComponents.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  handleClickButton = () => {
+    alert("click Me");
+  };
+
+  render() {
+    return (
+      <>
+        <div>Hello Son Đào</div>
+        <div>Hello Son Đào</div>
+        <div className="btn-box">
+          <button onClick={() => this.handleClickButton()}>Submit</button>
+        </div>
+      </>
+    );
+  }
+}
+
+export default MyComponent;
